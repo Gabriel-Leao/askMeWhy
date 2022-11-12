87ada9573113f54b188ccd8473aac0a4efe23683
Gabriel-Leao <gabriel.leao2507@gmail.com>
2022-11-12 13:18:40 -0300

    getting form informations with body-parser and partials done

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from 'express'
 import { routes } from './routes/index'
+import bodyParser from 'body-parser'
 
 const app = express()
 
 app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(routes)
+
 app.listen(3333, () => console.log('listen on port 3333!'))
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,5 +3,15 @@ import { Router } from 'express'
 export const routes = Router()
 
 routes.get('/', (req, res) => {
-  res.render('home/index')
+  res.render('pages/home')
+})
+
+routes.get('/ask', (req, res) => {
+  res.render('pages/ask')
+})
+
+routes.post('/send/question', (req, res) => {
+  const title = req.body.title
+  const description = req.body.description
+  res.send(`título: ${title} descrição: ${description}`)
 })
