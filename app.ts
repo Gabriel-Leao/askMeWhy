ce7d9e831bddddbcb42da2ef86bcf9e0fb303a0a
Gabriel-Leao <gabriel.leao2507@gmail.com>
2022-11-15 16:55:17 -0300

    ajustes

--- app.ts
+++ app.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import { connection } from './controllers/connection'
 import { routes } from './routes'
+import Ask from './models/Ask'
+import Answers from './models/Answer'
 
 const app = express()
 
@@ -13,6 +15,14 @@ connection
   .catch((error: Error) => {
     console.log(error.name)
   })
+
+Ask.sync({ force: false })
+  .then(() => console.log('Ask table successful created'))
+  .catch((error: Error) => console.log(error.name))
+
+Answers.sync({ force: false })
+  .then(() => console.log('Answers table successful created'))
+  .catch((error: Error) => console.log(error.name))
 
 app.set('view engine', 'ejs')
 
